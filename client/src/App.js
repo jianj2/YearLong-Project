0632667a8591ae5f30c781efdd338d40b90c559a
Waqas Rehmani <dada.scene@hotmail.com>
2020-05-10 22:43:20 +1000

    Set up react app.

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import Login from './screens/Login'
+
+
+function App() {
+  return (
+    <div className="App">
+      
+      <Login />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/screens/Login.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import '../styles/login.css'
+
+class Login extends Component {
+    
+    
+    
+    render() {
+        return(
+            <div className='login'>
+                Login
+            </div>
+        )
+    }
+}
+
+export default Login
