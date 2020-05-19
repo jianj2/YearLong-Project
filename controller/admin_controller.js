6e997ea188dd93e0570db8fdfee8f956e43afa25
VictorDing22 <dse250073@gmail.com>
2020-05-19 16:15:40 +1000

    Change login logic and use local json file to store Admin

--- controller/admin_controller.js
+++ controller/admin_controller.js
@@ -11,57 +11,82 @@
 
 
 const mongoose = require('mongoose');
+const adminFile = require('../file/admin')
 
-var Admin = mongoose.model('admin');
+var Admin = adminFile
+var Admin1 = mongoose.model('admin');
+
+// Define a message object returned to the front-end
+let responseData;
+responseData = {
+    // status
+    code: 0,
+    // message
+    message: ""
+};
+
 
 // Login check.
 var loginAdmin = function (req, res) {
-    var _username = "AdminExample"
-    var _password = "adminpassword"
-    Admin.findOne({username:_username}, function(err, user){
-        if(err){
-            console.log(err)
+    let _username = "AdminExample"
+    let _password = "adminpassword"
+    let username = Admin.username
+    let password = Admin.password
+    // Username can not be empty
+    if (_username === "" ){
+        responseData.code = 1;
+        responseData.message = "Username can not be empty!";
+        res.json(responseData);
+        return;
+    }
+    if (_password === "" ) {
+        responseData.code = 2;
+        responseData.message = "Password can not be empty!";
+        res.json(responseData);
+        return;
+    }
+    if (_username === username) {
+        // check out password
+        if (_password === password) {
+            responseData.code = 3
+            responseData.message = "Successfully login!";
+            res.json(responseData);
+            return;
+        } else {
+            responseData.code = 4;
+            responseData.message = "Password is incorrect!";
+            res.json(responseData);
+            return;
         }
-        if(!user){
-            console.log('wrong username')
-        }
-        else {
-            Admin.findOne({password:_password}, function (err,isMatch) {
-                if(err){
-                    console.log(err)
-                }
-                if(isMatch){
-                    console.log('successfully login')
-                } else{
-                    console.log('wrong password')
-                }
-            })
-        }
+    }else {
+        responseData.code = 5;
+        responseData.message = "Username does not exist!";
+        res.json(responseData);
+        return;
+    }
 
-        
-    });
 };
 
 // Create a new admin.
-var createAdmin = function (req,res) {
-    var newAdmin = new Admin({
-        adminId: 'admin',
-        name: "Victor",
-        username: "AdminExample",
-        email: "testadmin@example.com",
-        password: "adminpassword",
-        questionnaires: ['questionnaireId1', 'questionnaireId2'],
-    });
-
-    newAdmin.save(function(err, createdAdmin) {
-        if (!err){
-            res.send(createdAdmin);
-        } else {
-            res.send(err);
-        }
-    })
-};
+// var createAdmin = function (req,res) {
+//     var newAdmin = new Admin({
+//         adminId: 'admin',
+//         name: "Victor",
+//         username: "AdminExample",
+//         email: "testadmin@example.com",
+//         password: "adminpassword",
+//         questionnaires: ['questionnaireId1', 'questionnaireId2'],
+//     });
+//
+//     newAdmin.save(function(err, createdAdmin) {
+//         if (!err){
+//             res.send(createdAdmin);
+//         } else {
+//             res.send(err);
+//         }
+//     })
+// };
 
 
 module.exports.loginAdmin = loginAdmin;
-module.exports.createAdmin = createAdmin;
+//module.exports.createAdmin = createAdmin;
