d2b2f0adf414f6de63ea15857c6959c062b7d20c
uvinabeysinghe <uvinabeysinghe@users.noreply.github.com>
2020-05-24 22:49:00 +1000

    Improved email.js.

--- routes/email.js
+++ routes/email.js
@@ -1,11 +1,11 @@
 /**
  * ===============================================
- * ROUTES OF QUESTIONNAIRES (USING EXPRESS.ROUTER)
+ * ROUTES OF EMAIL (USING EXPRESS.ROUTER)
  * ===============================================
  * @date created: 24 May 2020
  * @authors: Uvin Abeysinghe
  *
- * The routes/questionnaire.js is used for defining the routes of each API call in questionnaire_controller made from the frontend.
+ * The routes/email.js is used for defining the routes of each API call in email_controller made from the frontend.
  */
 
 
@@ -13,8 +13,7 @@ var express = require('express');
 var router = express.Router();
 var email_controller = require('../controller/email_controller.js');
 
-// CRUD Routes for Posts
-router.post('/send', email_controller.send);
-
+// CRUD Routes for Emails
+router.post('/submit', email_controller.send);
 
 module.exports = router;
