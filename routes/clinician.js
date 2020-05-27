7f0d8a271403510fa3d756d343c72e62f62ba1b3
uvinabeysinghe <uvinabeysinghe@users.noreply.github.com>
2020-05-27 20:24:19 +1000

    Clinician to Parent/Child email working.

--- routes/clinician.js
+++ routes/clinician.js
@@ -16,6 +16,6 @@ var clinician_controller = require('../controller/clinician_controller.js');
 // CRUD Routes for Posts
 router.get('/', clinician_controller.getAllClinician);
 //router.get('/123', clinician_controller.createClinician);
-
+router.post("/share", clinician_controller.shareQuestionnaire);
 
 module.exports = router;
