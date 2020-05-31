78b0a7e512343c604583902c2ae572f6b7945471
uvinabeysinghe <uvinabeysinghe@users.noreply.github.com>
2020-05-31 14:18:25 +1000

    Created the Share Model.

---
+++ models/share.js
@@ -0,0 +1,21 @@
+/**
+ * =======================================
+ * SCHEMA OF SHARE (WITH MONGOOSE)
+ * =======================================
+ * @date created: 31 May 2020
+ * @authors: Uvin Abeysinghe
+ *
+ * The models/share.js is used for establishing the 'share' schema and types using mongoose.
+ */
+
+const mongoose = require('mongoose');
+
+const shareSchema = mongoose.Schema({
+    shareId: { type: String, required:true, lowercase:true, unique:true, trim:true},
+    clinicianEmail: {type: String, required:true, unique:true },
+    patientEmail: {type: String, required:true, unique:true },
+    questionnaireId: {type: String, required:true, unique:true },
+
+});
+
+module.exports = mongoose.model('share', shareSchema);
