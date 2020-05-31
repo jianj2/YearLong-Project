8a438a4850769b5f6bc0ec7a0b53e0268ad622c9
catherine <catherinejsong96@gmail.com>
2020-05-31 21:37:19 +1000

    Fixed spelling for custom questionnaires

--- client/src/components/Clinician/ManageQuestionnaires.js
+++ client/src/components/Clinician/ManageQuestionnaires.js
@@ -139,7 +139,7 @@ const ManageQuestionnaires = (props) => {
             </div>
             <div className="customized-questionnaire-container">
                 <div className="CQ-header">
-                    My Customised Questionnaires
+                    My Custom Questionnaires
                     <button className="Add-btn" onClick={AddNew}>
                         Add New
                     </button>
