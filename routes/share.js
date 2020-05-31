f3963eb32763f16e046732d8ba7ff72f061084a6
uvinabeysinghe <uvinabeysinghe@users.noreply.github.com>
2020-05-31 18:24:31 +1000

    Created a route for Share.

---
+++ routes/share.js
@@ -0,0 +1,19 @@
+/**
+ * ===============================================
+ * ROUTES OF SHARE (USING EXPRESS.ROUTER)
+ * ===============================================
+ * @date created: 31 May 2020
+ * @authors: Uvin Abeysinghe
+ *
+ * The routes/share.js is used for defining the routes of each API call in share_controller.
+ */
+
+
+var express = require('express');
+var router = express.Router();
+var share_controller = require('../controller/share_controller.js');
+
+// CRUD Routes for Shares
+router.get('/', share_controller.getQuestionnaireId());
+
+module.exports = router;
