fd1c792ea1d3dc50ff187dfbdf4a365695410105
uvinabeysinghe <uvinabeysinghe@users.noreply.github.com>
2020-05-31 17:33:09 +1000

    Created share_controller.js.

---
+++ controller/share_controller.js
@@ -0,0 +1,35 @@
+/**
+ * ========================================
+ * DEFINING SHARE API CALLS CONTROLLER
+ * ========================================
+ * @date created: 31 May 2020
+ * @authors: Uvin
+ *
+ * The share_controller is used for defining the functionality of api calls related to share.
+ *
+ */
+
+const mongoose = require("mongoose");
+
+// Create a new admin.
+// var createAdmin = function (req,res) {
+//     var newAdmin = new Admin({
+//         adminId: 'admin',
+//         name: "Victor",
+//         username: "AdminExample",
+//         email: "testadmin@example.com",
+//         password: "adminpassword",
+//         questionnaires: ['questionnaireId1', 'questionnaireId2'],
+//     });
+//
+//     newAdmin.save(function(err, createdAdmin) {
+//         if (!err){
+//             res.send(createdAdmin);
+//         } else {
+//             res.send(err);
+//         }
+//     })
+// };
+
+
+//module.exports.createAdmin = createAdmin;
