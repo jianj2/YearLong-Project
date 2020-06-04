98b6e97a6218c4c487a45bd8d910a87db2553631
Waqas Rehmani <dada.scene@hotmail.com>
2020-06-05 01:30:13 +1000

    Share Feature completed.

--- src/components/Clinician/ManageQuestionnaires.js
+++ src/components/Clinician/ManageQuestionnaires.js
@@ -12,7 +12,8 @@
  */
 
 import React, { useState, useEffect } from "react";
-
+import { Modal, Backdrop, Fade, FormControl, InputLabel, Input, FormHelperText, FormControlLabel, Checkbox } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 
 // Import Utils.
 import * as API from "../../utils/api";
@@ -25,13 +26,36 @@ import "../../styles/main.css";
 import QuestionnaireList from "../QuestionnaireList";
 import Loading from "../Loading";
 
-const ManageQuestionnaires = (props) => {
-    const { isAuthenticated, loginWithRedirect, user } = useAuth0();
-    console.log(user.name); //TODO: change that when we have actual clincianId
+const useStyles = makeStyles((theme) => ({
+    modal: {
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    paper: {
+        backgroundColor: theme.palette.background.paper,
+        border: "2px solid #000",
+        boxShadow: theme.shadows[5],
+        padding: theme.spacing(2, 4, 3),
+    },
+}));
 
+const ManageQuestionnaires = (props) => {
+    const classes = useStyles();
+    const { isAuthenticated, loginWithRedirect, user } = useAuth0();
+    // console.log("user.name", user.name); //TODO: change that when we have actual clincianId
 
     const [questionnaires, setQuestionnaires] = useState([]);
     const [loading, setLoading] = useState(false);
+
+    const [isShareModalVisible, setIsShareModalVisible] = useState(false);
+    const [shareModalData, setShareModalData] = useState({
+        patientEmail: "",
+        questionnaireId: "",
+        clinicianEmail: user.name,
+        message: "",
+        readOnly: false
+    });
 
     useEffect(() => {
         setLoading(true);
@@ -46,7 +70,7 @@ const ManageQuestionnaires = (props) => {
             setQuestionnaires(customisedQuestionnaires);
             setLoading(false);
         }
-        
+
         retrieveQuestionnaires();
     }, []);
 
@@ -73,7 +97,7 @@ const ManageQuestionnaires = (props) => {
     const editQuestionnaire = (questionnaireID) => {
         let edit_url = "/clinician/" + questionnaireID + "/edit";
         window.location.href = edit_url;
-    }
+    };
 
     // Function called when Delete is clicked on the QuestionnaireList
     const deleteQuestionnaire = (questionnaireId) => {
@@ -84,15 +108,20 @@ const ManageQuestionnaires = (props) => {
     };
 
     // Function called when Share is clicked on the QuestionnaireList
-    const shareQuestionnaire = (questionnaireID) => {
-
-    }
+    const shareQuestionnaire = (questionnaireId) => {
+        console.log("share Questionnaire ", questionnaireId);
+        setShareModalData({
+            ...shareModalData,
+            questionnaireId,
+        });
+        openModal();
+    };
 
     // Function called when Add New Button is clicked
     async function AddNew() {
         setLoading(true);
         const uuid = await API.addQuestionnaire(user.name);
-        
+
         // const today = formatDate();
         const AddedArray = questionnaires;
         let newQuestionnaire = {
@@ -101,17 +130,107 @@ const ManageQuestionnaires = (props) => {
             description: "Please click edit to begin with this questionnaire.",
             sections: [],
             isStandard: false,
-        }; 
+        };
         setQuestionnaires([newQuestionnaire, ...questionnaires]);
         setLoading(false);
         // let edit_url = "/clinician/" + uuid + "/edit";
         // window.location.href = edit_url;
     }
- 
+
+    // ========================================================================
+    // Share Modal Functions
+    // ========================================================================
+    const openModal = () => setIsShareModalVisible(true);
+    const closeModal = () => setIsShareModalVisible(false);
+
+    const handleShareSubmit = (e) => {
+        e.preventDefault();
+        setLoading(true); 
+        API.shareQuestionnaire(shareModalData).then( res => {
+            console.log("printing the res: ", res);
+            setLoading(false); 
+            closeModal();
+        })
+    }
+
+    const renderShareModal = () => {
+        return (
+            <Modal
+                open={isShareModalVisible}
+                onClose={closeModal}
+                closeAfterTransition
+                className={classes.modal}
+                BackdropComponent={Backdrop}
+                BackdropProps={{
+                    timeout: 500,
+                }}
+            >
+                <Fade in={isShareModalVisible}>
+                    <form className="share-modal-container" onSubmit={handleShareSubmit}>
+                        <h2>Share Details</h2>
+                        <FormControl margin="dense">
+                            <InputLabel>Patient's Email</InputLabel>
+                            <Input
+                                onChange={(e) => {
+                                    setShareModalData({
+                                        ...shareModalData,
+                                        patientEmail: e.target.value,
+                                    });
+                                }}
+                                name="patientEmail"
+                                type="email"
+                                required
+                            />
+                            <FormHelperText>Please enter patient's email.</FormHelperText>
+                        </FormControl>
+
+                        <FormControl margin="dense">
+                            <InputLabel>Personalised Message</InputLabel>
+                            <Input
+                                onChange={(e) => {
+                                    setShareModalData({
+                                        ...shareModalData,
+                                        message: e.target.value,
+                                    });
+                                }}
+                                multiline
+                                name="message"
+                                type="text"
+                            />
+                            <FormHelperText>Please enter a personalised Message that you want to send to the patient (optional).</FormHelperText>
+                        </FormControl>
+
+                        <FormControl>
+                            <FormControlLabel
+                                control={
+                                    <Checkbox
+                                        checked={!shareModalData.readOnly}
+                                        onChange={() => {
+                                            setShareModalData({
+                                                ...shareModalData,
+                                                readOnly: !shareModalData.readOnly,
+                                            });
+                                        }}
+                                    />
+                                }
+                                label="Allow parent to complete the questionnaire"
+                            />
+                            <FormHelperText>
+                                If this option is checked, patients can independently complete the questionnaire and the report would be sent to you.
+                            </FormHelperText>
+                        </FormControl>
+                        <button className="button">S H A R E</button>
+                    </form>
+                </Fade>
+            </Modal>
+        );
+    };
 
     return (
         <div>
             {loading ? <Loading /> : null}
+
+            {renderShareModal()}
 
             <div className="standard-questionnaire-container">
                 <div className="SQ-header">
@@ -126,7 +245,7 @@ const ManageQuestionnaires = (props) => {
                 <button className="button" onClick={AddNew}>
                     A D D &nbsp; N E W
                 </button>
-            </div> 
+            </div>
 
             <QuestionnaireList
                 questionnaires={questionnaires}
