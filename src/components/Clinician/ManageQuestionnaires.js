6a96cf7fa67a3a2bec3ff3265e3d2ec5850c8740
Waqas Rehmani <dada.scene@hotmail.com>
2020-06-05 00:27:13 +1000

    Add New, Delete Questionnaire Buttons working. Questionnaire is added to the top of the list now.

--- src/components/Clinician/ManageQuestionnaires.js
+++ src/components/Clinician/ManageQuestionnaires.js
@@ -13,25 +13,28 @@
 
 import React, { useState, useEffect } from "react";
 
-// Import styles.
-import "../../styles/managequestionnaires.css";
-import "../../styles/main.css";
+
+// Import Utils.
 import * as API from "../../utils/api";
 import { formatDate } from "../../utils/formatter";
 import { useAuth0 } from "../../utils/react-auth0-spa";
+// Import styles.
+import "../../styles/managequestionnaires.css";
+import "../../styles/main.css";
+// Import Components.
 import QuestionnaireList from "../QuestionnaireList";
+import Loading from "../Loading";
 
 const ManageQuestionnaires = (props) => {
-    const { loading, isAuthenticated, loginWithRedirect, user } = useAuth0();
+    const { isAuthenticated, loginWithRedirect, user } = useAuth0();
     console.log(user.name); //TODO: change that when we have actual clincianId
 
-    const [Questionnaires, setQuestionnaires] = useState({
-        customized_Questionnaire: [],
-    });
 
-    const [customQuestionnaires, setCustomQuestionnaires] = useState([]);
+    const [questionnaires, setQuestionnaires] = useState([]);
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
+        setLoading(true);
         async function retrieveQuestionnaires() {
             const customisedQuestionnaires = await API.getClinicianQuestionnaires(user.name);
             console.log(customisedQuestionnaires);
@@ -39,9 +42,11 @@ const ManageQuestionnaires = (props) => {
             const customisedQuestionnairesElement = customisedQuestionnaires.map((q) => {
                 return { QID: q.questionnaireId, Qname: q.title, Qdescription: q.description, date: today };
             });
-            setQuestionnaires({ customized_Questionnaire: customisedQuestionnairesElement });
-            setCustomQuestionnaires(customisedQuestionnaires);
+            // setQuestionnaires({ customized_Questionnaire: customisedQuestionnairesElement });
+            setQuestionnaires(customisedQuestionnaires);
+            setLoading(false);
         }
+        
         retrieveQuestionnaires();
     }, []);
 
@@ -55,14 +60,14 @@ const ManageQuestionnaires = (props) => {
         );
     }
 
-    function CQlist() {
-        var customized_Questionnaire_list = [];
-        var q;
-        for (q of Questionnaires.customized_Questionnaire) {
-            customized_Questionnaire_list.push(CQgenerator(q.QID, q.Qname, q.Qdescription, q.date));
-        }
-        return customized_Questionnaire_list;
-    }
+    // function CQlist() {
+    //     var customized_Questionnaire_list = [];
+    //     var q;
+    //     for (q of Questionnaires.customized_Questionnaire) {
+    //         customized_Questionnaire_list.push(CQgenerator(q.QID, q.Qname, q.Qdescription, q.date));
+    //     }
+    //     return customized_Questionnaire_list;
+    // }
 
     // Function called when Edit is clicked on the QuestionnaireList
     const editQuestionnaire = (questionnaireID) => {
@@ -71,54 +76,43 @@ const ManageQuestionnaires = (props) => {
     }
 
     // Function called when Delete is clicked on the QuestionnaireList
-    const deleteQuestionnaire = (questionnaireID) => {
-        const arrayCopy = Questionnaires.customized_Questionnaire.filter((q) => q.QID !== questionnaireID);
-        setQuestionnaires({ customized_Questionnaire: arrayCopy });
-        API.deleteQuestionnaire(questionnaireID, user.name);
-    }
+    const deleteQuestionnaire = (questionnaireId) => {
+        console.log("delete ", questionnaireId);
+        const arrayCopy = questionnaires.filter((q) => q.questionnaireId !== questionnaireId);
+        setQuestionnaires(arrayCopy);
+        API.deleteQuestionnaire(questionnaireId, user.name);
+    };
 
     // Function called when Share is clicked on the QuestionnaireList
     const shareQuestionnaire = (questionnaireID) => {
-        
+
     }
 
+    // Function called when Add New Button is clicked
     async function AddNew() {
+        setLoading(true);
         const uuid = await API.addQuestionnaire(user.name);
-        const today = formatDate();
-        const AddedArray = Questionnaires.customized_Questionnaire;
-        AddedArray.push({
-            QID: uuid,
-            Qname: "New custom questionnaire",
-            Qdescription: "Provide some description for this questionnaire.",
-            date: today,
-        });
-        setQuestionnaires({ customized_Questionnaire: AddedArray });
-
+        
+        // const today = formatDate();
+        const AddedArray = questionnaires;
+        let newQuestionnaire = {
+            questionnaireId: uuid,
+            title: "New Questionnaire",
+            description: "Please click edit to begin with this questionnaire.",
+            sections: [],
+            isStandard: false,
+        }; 
+        setQuestionnaires([newQuestionnaire, ...questionnaires]);
+        setLoading(false);
         // let edit_url = "/clinician/" + uuid + "/edit";
         // window.location.href = edit_url;
     }
-
-    function CQgenerator(QID, Qname, Qdescription, date) {
-        // var edit_url = "/clinician/" + QID + "/edit";
-        return (
-            <div className="q-frame" key={QID}>
-                <div className="q-name">{Qname}</div>
-                <div className="q-description">{Qdescription}</div>
-                <div className="date">{date}</div>
-                <div className="btns-container">
-                    <button className="edit-btn" onClick={(e) => editQuestionnaire(QID, e)}>
-                        Edit
-                    </button>
-                    <button className="delete-btn" onClick={(e) => deleteQuestionnaire(QID, e)}>
-                        Delete
-                    </button>
-                </div>
-            </div>
-        );
-    }
+ 
 
     return (
         <div>
+            {loading ? <Loading /> : null}
+
             <div className="standard-questionnaire-container">
                 <div className="SQ-header">
                     <h1>Standard questionnaires</h1>
@@ -126,20 +120,17 @@ const ManageQuestionnaires = (props) => {
                 {SQgenerator("SSQ-P", "SSQ for parents", "17/05/2020")}
                 {SQgenerator("SSQ-C", "SSQ for children ", "17/05/2020")}
             </div>
-            {/* <div className="customized-questionnaire-container">
-                <div className="CQ-header">
-                    <h1>My Custom Questionnaires</h1>
-                    <button className="Add-btn" onClick={AddNew}>
-                        Add New
-                    </button>
-                </div>
-                
-                <CQlist />
-            </div> */}
+
+            <div className="CQ-header">
+                <h1>My Questionnaires</h1>
+                <button className="button" onClick={AddNew}>
+                    A D D &nbsp; N E W
+                </button>
+            </div> 
 
             <QuestionnaireList
-                questionnaires={customQuestionnaires}
-                listTitle={"My Questionnaires"}
+                questionnaires={questionnaires}
+                listTitle={""}
                 isSelectable={false}
                 onClickQuestion={() => {}}
                 canEdit={true}
