3e8afd6ef829d57907f613cdb6259a673f29a727
jianj2 <jianj2@student.unimelb.edu.au>
2020-08-23 21:34:51 +1000

    make the scale look more like a ruler

--- src/components/Question.js
+++ src/components/Question.js
@@ -52,6 +52,37 @@ export default function Question({
         value: undefined,
         supplementaryValue: "",
     });
+
+    const marks = [
+        {
+            value: 1,
+            label: '1',
+        },{
+            value: 2,
+            label: '2',
+        },{
+            value: 3,
+            label: '3',
+        },{
+            value: 4,
+            label: '4',
+        },{
+            value: 5,
+            label: '5',
+        },{
+            value: 6,
+            label: '6',
+        },{
+            value: 7,
+            label: '7',
+        },{
+            value: 8,
+            label: '8',
+        },{
+            value: 9,
+            label: '9',
+        },
+    ];
     
 
     // useEffect(() => {
@@ -113,6 +144,7 @@ export default function Question({
                                 value: val,
                             })
                         }
+                        marks={marks}
                         min={0}
                         max={10}
                         disabled={readOnly}
@@ -192,6 +224,7 @@ export default function Question({
                                 value: val,
                             })
                         }
+                        marks={marks}
                         min={0}
                         max={10}
                         disabled={readOnly}
