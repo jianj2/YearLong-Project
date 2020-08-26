d380b38e459d57a07be253f082c11bd722005e9b
Mayank Sharma <mayankshar21@yahoo.com.sg>
2020-08-26 17:32:12 +1000

    Changing IP

--- src/screens/HomeClinician.js
+++ src/screens/HomeClinician.js
@@ -39,7 +39,7 @@ const HomeClinician = (props) => {
         }
         const fn = async () => {
             await loginWithRedirect({
-                redirect_uri: "http://52.65.44.7:3000/", //TODO: figure out why window.location.pathname doesn't work
+                redirect_uri: "http://localhost:3000/clinician", //TODO: figure out why window.location.pathname doesn't work
                 //appState: { targetUrl: window.location.pathname},
             });
         };
