899408cf9ac0063cad0a27698a09b9b2ee1be993
uvinabeysinghe <uvinabeysinghe@users.noreply.github.com>
2020-08-27 14:22:44 +1000

    Revert "hot fix - guarantee error-free loading"
    This reverts commit a98785da

--- src/components/Questionnaire.js
+++ src/components/Questionnaire.js
@@ -14,7 +14,6 @@
 import React, { useState, useEffect } from "react";
 import { useForm } from "react-hook-form";
 import { Slider } from "@material-ui/core";
-import Loading from "./Loading";
 
 // Import components.
 import Question from "./Question";
@@ -23,22 +22,24 @@ import Question from "./Question";
 import "../styles/questionnaire.css";
 import "../styles/main.css";
 
+
+
 export default function Questionnaire({
     readOnly,
     questionnaire,
     submitQuestionnaire,
     questionnaireData,
-    handleQuestionnaireChange,
-    sectionVisibility,
+    handleQuestionnaireChange,sectionVisibility
 }) {
     const { register, handleSubmit, errors } = useForm();
-    const [loading, setLoading] = useState(true);
+
 
     // Method: Called when we submit the questionnaire
     const onSubmit = (e) => {
         e.preventDefault();
         submitQuestionnaire();
     };
+
 
     // Method: Called when we something in the questionnaire changes.
     const onQuestionChange = (
@@ -47,61 +48,54 @@ export default function Questionnaire({
         questionIndex,
         data
     ) => {
-        handleQuestionnaireChange(
-            sectionIndex,
-            scenarioIndex,
-            questionIndex,
-            data
-        );
+        handleQuestionnaireChange(sectionIndex, scenarioIndex, questionIndex, data);
     };
+
 
     //////////// Share section update /////////////////////////////
     // get a list of the visible sections
-    const getVisibleSections = (sections, visibilityInfoList) => {
+    const getVisibleSections = (sections, visibilityInfoList ) => {
+
         const filteredSections = sections.filter((section) => {
-            const foundVisibilityInfo = visibilityInfoList.find(
-                (visibilityInfo) => {
-                    return visibilityInfo.title === section.title;
+                const foundVisibilityInfo = visibilityInfoList.find((visibilityInfo) => {
+                    return (visibilityInfo.title === section.title)
+                });
+                if (foundVisibilityInfo != undefined){
+                    return foundVisibilityInfo.isVisible
+                }else{
+                    return null
                 }
-            );
-            if (foundVisibilityInfo != undefined) {
-                return foundVisibilityInfo.isVisible;
-            } else {
-                return null;
+
             }
-        });
+        );
         return filteredSections;
-    };
+
+    }
 
     // set the updates questionnaire sections.
-    const updateSections = (questionnaire, sectionVisibility) => {
-        console.log(sectionVisibility);
-        if (sectionVisibility != undefined) {
-            questionnaire.sections = getVisibleSections(
-                questionnaire.sections,
-                sectionVisibility
-            );
+    const updateSections = ((questionnaire, sectionVisibility) =>{
+        console.log(sectionVisibility)
+        if (sectionVisibility != undefined ){
+
+            questionnaire.sections = getVisibleSections(questionnaire.sections, sectionVisibility)
         }
-    };
 
-    updateSections(questionnaire, sectionVisibility);
-    setLoading(false);
+    })
+
+    updateSections(questionnaire, sectionVisibility)
     //////////////////////////////////////////////////////////////
-    if (loading) {
-        return <Loading />;
-    } else {
-        return (
-            <form onSubmit={onSubmit} className="questionaire-container">
-                <h1>{questionnaire.title}</h1>
 
-                {questionnaire.sections.map((section, sectionIndex) => (
+    return (
+        <form onSubmit={onSubmit} className="questionaire-container">
+            <h1>{questionnaire.title}</h1>
+
+            {questionnaire.sections.map((section, sectionIndex) => (
+
+
                     <div key={sectionIndex} className="section-container">
                         <h2>{section.title}</h2>
                         {section.scenarios.map((scenario, scenarioIndex) => (
-                            <div
-                                key={scenarioIndex}
-                                className="scenario-container"
-                            >
+                            <div key={scenarioIndex} className="scenario-container">
                                 <p>{scenario.description}</p>
                                 {scenario.questions.map(
                                     (question, questionIndex) => (
@@ -120,7 +114,7 @@ export default function Questionnaire({
                                             data={
                                                 questionnaireData[sectionIndex][
                                                     scenarioIndex
-                                                ][questionIndex]
+                                                    ][questionIndex]
                                             }
                                         />
                                     )
@@ -128,21 +122,27 @@ export default function Questionnaire({
                             </div>
                         ))}
                     </div>
-                ))}
 
-                <div className="questionaire-submit-button">
-                    {readOnly ? null : (
-                        <button
-                            id="review"
-                            className={
-                                errors.code ? "button-disabled" : "button"
-                            }
-                        >
-                            R E V I E W
-                        </button>
-                    )}
-                </div>
-            </form>
-        );
-    }
+                )
+
+
+
+
+
+
+            )}
+
+            <div className="questionaire-submit-button">
+                {readOnly ? null : (
+                    <button
+                        id="review"
+                        className={errors.code ? "button-disabled" : "button"}
+                    >
+                        R E V I E W
+                    </button>
+                )}
+            </div>
+        </form>
+    );
+    // }
 }
