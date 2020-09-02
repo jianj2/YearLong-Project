13ec061dc2625a0aaadc9f0160cdc978300cdff9
caryjpaepen <42157570+caryjpaepen@users.noreply.github.com>
2020-09-02 22:29:06 +1000

    set up logic to use summary api

--- src/components/Admin/SSQInstructionsList.js
+++ src/components/Admin/SSQInstructionsList.js
@@ -10,10 +10,23 @@
  */
 
 import SSQInstructionContainer from "./SSQInstructionContainer";
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import {getInstructionsSummary} from "../../utils/api";
 const SSQInstructionsList = () => {
 
+const [instructionsSummary, setInstructionsSummary] = useState([]); 
+const setup = async ()  => {
+    const summary = await getInstructionsSummary();
+    setInstructionsSummary(summary);
+
+
+};
+useEffect(()=>{
+    setup();
+},[]);
+
+
+console.log(instructionsSummary);
 return (<SSQInstructionContainer 
 instructionType = "CC"/>);
 
