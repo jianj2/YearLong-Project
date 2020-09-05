09673ba1f8081215d7803a6fe33f4f5f5473530a
uvinabeysinghe <uvinabeysinghe@users.noreply.github.com>
2020-09-05 13:08:27 +1000

    Added micro-ticks.

--- src/components/Question.js
+++ src/components/Question.js
@@ -33,51 +33,69 @@ import "../styles/main.css";
 const SliderWithTicks = withStyles({
     mark: {
         backgroundColor: "rgba(245, 0, 87, 0.7)",
-        height: 15,
+        height: 12,
         width: 1.5,
         marginTop: -3,
         marginLeft:-1
+    },
+    markActive: {
+        opacity: 1,
+        backgroundColor: "rgba(255,255,255, 0.3)"
+    },
+    markLabelActive:{
+        opacity: 1,
+        color: "rgba(255,255,255, 0.3)"
     }
 })(Slider);
 
 
-const marks = [
-    {
-        value: 0,
-        label: '0',
-    }, {
-        value: 1,
-        label: '1',
-    },{
-        value: 2,
-        label: '2',
-    },{
-        value: 3,
-        label: '3',
-    },{
-        value: 4,
-        label: '4',
-    },{
-        value: 5,
-        label: '5',
-    },{
-        value: 6,
-        label: '6',
-    },{
-        value: 7,
-        label: '7',
-    },{
-        value: 8,
-        label: '8',
-    },{
-        value: 9,
-        label: '9',
-    },
-    ,{
-        value: 10,
-        label: '10',
-    },
-];
+const createMarks = function (){
+    let mymarks = [
+        {
+            value: 0,
+            label: '0',
+        }, {
+            value: 1,
+            label: '1',
+        },{
+            value: 2,
+            label: '2',
+        },{
+            value: 3,
+            label: '3',
+        },{
+            value: 4,
+            label: '4',
+        },{
+            value: 5,
+            label: '5',
+        },{
+            value: 6,
+            label: '6',
+        },{
+            value: 7,
+            label: '7',
+        },{
+            value: 8,
+            label: '8',
+        },{
+            value: 9,
+            label: '9',
+        },
+        ,{
+            value: 10,
+            label: '10',
+        },
+    ];
+    let i;
+    for ( i = 0; i<=10; i+=0.1 ){
+        let temp = {value: i, style:{color:'blue'}}
+        mymarks.push(temp)
+    }
+    return mymarks;
+}
+
+const marks = createMarks();
 
 
 
