ddd8d1a6184940089b158561c99e180ca14978ac
caryjpaepen <42157570+caryjpaepen@users.noreply.github.com>
2020-09-09 18:34:35 +1000

    remove test api

--- app.js
+++ app.js
@@ -1,35 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const jwt = require('express-jwt');
-const jwksRsa = require('jwks-rsa');
 
 
 const app = express();
-const domain = "http://localhost:3001";
-
-// Authentication middleware. When used, the
-// Access Token must exist and be verified against
-// the Auth0 JSON Web Key Set
-const checkJwt = jwt({
-    // Dynamically provide a signing key
-    // based on the kid in the header and 
-    // the signing keys provided by the JWKS endpoint.
-    secret: jwksRsa.expressJwtSecret({
-      cache: true,
-      rateLimit: true,
-      jwksRequestsPerMinute: 5,
-      jwksUri: `https://pediatric-scale.au.auth0.com/.well-known/jwks.json`
-    }),
-  
-    // Validate the audience and the issuer.
-    audience: `${domain}/clinician`,
-    issuer: `https://pediatric-scale.au.auth0.com/`,
-    algorithms: ['RS256']
-  });
-
-  
-
-
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -39,14 +12,8 @@ require('./models/db.js');
 
 // CORS
 const cors = require('cors');
-
 app.use(cors());
 
-app.get('/admin/secret2', checkJwt, function(req, res) {
-    res.json({
-      message: 'Hello from a private endpoint! You need to be authenticated and have a scope of read:messages to see this.'
-    });
-  });
 // defining routes
 const indexRouter = require('./routes');
 const clinicianRouter = require('./routes/clinician');
