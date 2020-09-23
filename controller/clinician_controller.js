fb9263db8eb80ea9e41f1be25a4a37e1062fa40f
Waqas Rehmani <dada.scene@hotmail.com>
2020-09-23 22:29:54 +1000

    Fix response.

--- controller/clinician_controller.js
+++ controller/clinician_controller.js
@@ -60,8 +60,8 @@ const completeQuestionnaire = function (req, res) {
     const userEmail = extractUserEmail(req);
     if(userEmail === clinicianEmail){
         sendResultsEmail(questionnaireId, questionnaireData, clinicianEmail, personalDetails, sortBy)
-        // .then(emailRes => res.send(emailRes))
-        // .catch(emailRej => res.send(emailRej));
+        .then(emailRes => res.send(emailRes))
+        .catch(emailRej => res.send(emailRej));
     }else{
         res.send(JSON.stringify("Authorisation failed."));
     }
