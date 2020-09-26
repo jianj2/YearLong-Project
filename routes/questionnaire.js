ef98c90842e25bf19a51ad771d63fb9873b35e73
caryjpaepen <42157570+caryjpaepen@users.noreply.github.com>
2020-09-26 17:07:17 +1000

    use NODE_ENV to determine Auth0 tenant

--- routes/questionnaire.js
+++ routes/questionnaire.js
@@ -14,14 +14,30 @@ const router = express.Router();
 const questionnaire_controller = require('../controller/questionnaire_controller.js');
 const jwt = require('express-jwt');
 const jwksRsa = require('jwks-rsa');
-const domain = //process.env.SERVER || 
-"http://localhost:3001";
+const domain = process.env.SERVER || "http://localhost:3001";
 
 
 // Authentication middleware. When used, the
 // Access Token must exist and be verified against
 // the Auth0 JSON Web Key Set
-const checkJwt = jwt({
+const production = process.env.NODE_ENV;
+const checkJwt = production == "production" ? jwt({
+  // Dynamically provide a signing key
+  // based on the kid in the header and 
+  // the signing keys provided by the JWKS endpoint.
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://ssq.au.auth0.com/.well-known/jwks.json` 
+  }),
+
+  // Validate the audience and the issuer.
+  audience: `${domain}/clinician`,
+  issuer: `https://ssq.au.auth0.com/`,
+  algorithms: ['RS256']
+}):
+jwt({
     // Dynamically provide a signing key
     // based on the kid in the header and 
     // the signing keys provided by the JWKS endpoint.
