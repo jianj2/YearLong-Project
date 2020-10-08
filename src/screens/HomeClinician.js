8c4b85911079d7e8527bffd3cbc8257eefbacd23
caryjpaepen <42157570+caryjpaepen@users.noreply.github.com>
2020-10-08 12:55:59 +1100

    hot-fix resolve registration failure on localhost

--- src/screens/HomeClinician.js
+++ src/screens/HomeClinician.js
@@ -29,75 +29,79 @@ import ContentPanel from "../components/Clinician/ContentPanel";
 // This function defines the Clinician Home screen.
 // ---------------------------------------------------------------
 const HomeClinician = (props) => {
-    const { loading, isAuthenticated, loginWithRedirect, user, getTokenSilently, setToken} = useAuth0();
-    
+    const {
+        loading,
+        isAuthenticated,
+        loginWithRedirect,
+        user,
+        getTokenSilently,
+        getTokenWithPopup,
+        setToken,
+    } = useAuth0();
+
     const domain = process.env.REACT_APP_SERVER || "http://localhost:3001";
     const client = process.env.REACT_APP_CLIENT || "http://localhost:3000";
-     
+
     // this API must be listed in one of the Auth0 APIs
     const clinicianAuthAPI = `${domain}/clinician`;
-    
+
     useEffect(() => {
-        if (loading ) {
+        if (loading) {
             return;
         }
-        if (!loading && isAuthenticated){
-            const setAuth0Token = async ()=>{
+        if (!loading && isAuthenticated && user != null) {
+            let accessToken;
+            const setAuth0Token = async () => {
                 try {
-                const accessToken = await getTokenSilently({
-                    audience: clinicianAuthAPI,
-                    scope: "read:current_user",
-                  });
-                setToken(accessToken);
-                
-                }catch(e){
-                    console.log("error:", e);
+                    accessToken = await getTokenSilently({
+                        audience: clinicianAuthAPI,
+                        scope: "read:current_user",
+                    });
+                    setToken(accessToken);
+                } catch (e) {
+                    accessToken = await getTokenWithPopup({
+                        // this is for the first time when some registers on localhost (
+                        // localhost is treated differently by Auth0
+                        audience: clinicianAuthAPI,
+                        scope: "read:current_user",
+                    });
+                    setToken(accessToken);
+                    // console.log("error:", e);
                 }
-            }
+            };
             setAuth0Token();
             return;
         }
 
-        
         const fn = async () => {
-
             await loginWithRedirect({
+                redirect_uri: `${client}/clinician`,
 
-                redirect_uri: `${client}/clinician`
-                
                 //"http://localhost:3000/clinician", //TODO: figure out why window.location.pathname doesn't work
 
                 //redirect_uri: "https://d1hg2pgsuj0kio.cloudfront.net/clinician", //TODO: figure out why window.location.pathname doesn't work
 
                 //appState: { targetUrl: window.location.pathname},
             });
-            
-  
         };
 
         fn();
-    }, [isAuthenticated, loading, user, loginWithRedirect]);
+    }, [isAuthenticated, loading, user]);
 
     // const { loading, user } = useAuth0();
 
     if (loading || !user) {
         return <Loading />;
-    }else{
-        
+    } else {
     }
 
-
-  
-    
-
     return (
-     
         <div className="HomeClinician">
             <SideBar />
 
             <ContentPanel
                 active={props.active}
-                questionnaireID={props.questionnaireID} 
+                questionnaireID={props.questionnaireID}
             />
         </div>
     );
