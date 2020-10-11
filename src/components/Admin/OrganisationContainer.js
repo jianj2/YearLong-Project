c051e48d3d45956635e84e2c2a39652a25c81905
UvinAbeysinghe <dabeysinghe@student.unimelb.edu.au>
2020-10-11 15:28:15 +1100

    Resolve merge conflict.

--- src/components/Admin/OrganisationContainer.js
+++ src/components/Admin/OrganisationContainer.js
@@ -9,11 +9,13 @@ const OrganisationContainer = ({ organName }) => {
 
     useEffect(() => {
         const getAllClinicianInOrgan = async () => {
-            const [statusCode, clinicians] = await getOrganisationClinicians(organName);
-            if (statusCode === 200) {
-                const clinicianIds = clinicians.map((c) => c.clinicianId);
-                setAllClinicianOrgan(clinicianIds);
-            }
+            try {
+                const [statusCode, clinicians] = await getOrganisationClinicians(organName);
+                if (statusCode === 200) {
+                    const clinicianIds = clinicians.map((c) => c.clinicianId);
+                    setAllClinicianOrgan(clinicianIds);
+                }
+            }catch(e){}
         }
         getAllClinicianInOrgan();
     }, [organName])
