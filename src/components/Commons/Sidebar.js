b8dc9b8f08a6ffb0db394bee975cb427eec8e316
Waqas Rehmani <dada.scene@hotmail.com>
2020-10-12 21:23:13 +1100

    Create one common sidebar to be used everywhere.

--- src/components/Commons/Sidebar.js
+++ src/components/Commons/Sidebar.js
@@ -15,29 +15,62 @@
 import React from "react";
 import { Link } from 'react-router-dom'
 
-// Import styles.
+// Import Utilities
+import { USER_TYPE_ADMIN, USER_TYPE_CLINICIAN } from '../../utils/helper'
+
+// Import Styles
 import "../../styles/sidebar.css";
 
-
 // handles rendering of SideBar with Login
-const SideBar = ({  }) => {
+const SideBar = ({ userType }) => {
 
     const pathname = window.location.href.split("/").pop()
-
-    return (
-        <div className="sidebar-container">
-            {console.log(pathname)}
-            <Link className={`sidebar-questionnaires
+    if (userType === USER_TYPE_CLINICIAN) {
+        return (
+            <div className="sidebar-container">
+                <Link
+                    className={`sidebar-questionnaires
             ${(pathname === "clinician" || pathname === "Questionnaires" || pathname === "edit")? "active" : ""}`}
-                  to = "/clinician/Questionnaires">Questionnaires</Link>
-            <Link className={`sidebar-do-the-test ${(pathname === "DoTheTest")? "active" : ""}`}
-                  to = "/clinician/DoTheTest">Start a Questionnaire</Link>
-            <Link className={`sidebar-share ${(pathname === "Share")? "active" : ""}`}
-                  to = "/clinician/Share">Share a Questionnaire</Link>
-            <Link className={`sidebar-instructions ${(pathname === "Instructions")? "active" : ""}`}
-                  to = "/clinician/Instructions">Instructions</Link>
-        </div>
-    );
+                    to = "/clinician/Questionnaires">Questionnaires</Link>
+                <Link
+                    className={`sidebar-do-the-test ${(pathname === "DoTheTest")? "active" : ""}`}
+                    to = "/clinician/DoTheTest">Start a Questionnaire</Link>
+                <Link
+                    className={`sidebar-share ${(pathname === "Share")? "active" : ""}`}
+                    to = "/clinician/Share">Share a Questionnaire</Link>
+                <Link
+                    className={`sidebar-instructions ${(pathname === "Instructions")? "active" : ""}`}
+                    to = "/clinician/Instructions">Instructions</Link>
+            </div>
+        );
+    } else if (userType === USER_TYPE_ADMIN){
+        return (
+            <div className="sidebar-container">
+                <Link
+                    className={`sidebar-questionnaires
+                        ${(pathname === "admin" || pathname === "Questionnaires" || pathname === "edit") ? "active" : ""}`}
+                      to="/admin/Questionnaires"
+                >
+                    Questionnaires
+                </Link>
+                <Link
+                    className={`sidebar-ssq-instructions ${(pathname === "SSQ_Instructions") ? "active" : ""}`}
+                    to="/admin/SSQ_Instructions"
+                >
+                    SSQ Instructions
+                </Link>
+                <Link
+                    className={`sidebar-organisation ${(pathname === "Organisation" || pathname === "Country") ? "active" : ""}`}
+                    to="/admin/Country"
+                >
+                    Organisation
+                </Link>
+            </div>
+        )
+    } else {
+        return null
+    }
+
 };
 
 
