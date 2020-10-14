a51e704eb0fccf2c798537269024a697e52a9a95
Waqas Rehmani <dada.scene@hotmail.com>
2020-10-15 08:51:40 +1100

    ContentPanel now uses switch instead of if/else.

--- src/components/Commons/ContentPanel.js
+++ src/components/Commons/ContentPanel.js
@@ -18,119 +18,101 @@ import {
     ManageQuestionnaires,
     DoTheTestContainer,
     InstructionsContainer,
-    QuestionnaireContainer,
     ViewQuestionnaireContainer,
     ShareContainer
-} from '../../screens/Clinician'
+} from "../../screens/Clinician";
+
+import {
+    SSQInstructionsList,
+    CountryList,
+    AdminViewStandardQuestionnaire,
+    InstructionContainer,
+    OrganisationList,
+    OrganisationContainer,
+    AdminManageQuestionnaires
+} from "../../screens/Admin";
+
+import {
+    QuestionnaireContainer
+} from "../../screens/Common";
 
 // Import Styles
 import "../../styles/contentpanel.css";
 import "../../styles/main.css";
 
-import { USER_TYPE_CLINICIAN, USER_TYPE_ADMIN } from '../../utils/helper'
-import SSQInstructionsList from "../../screens/Admin/SSQInstructionsList";
-import CountryList from "../../screens/Admin/CountryList";
-import AdminViewStandardQuestionnaire
-    from "../../screens/Admin/AdminViewStandardQuestionnaire";
-import InstructionContainer from "../../screens/Admin/SSQInstructionContainer";
-import OrganisationList from "../../screens/Admin/OrganisationList";
-import OrganisationContainer from "../../screens/Admin/OrganisationContainer";
-import AdminManageQuestionnaires
-    from "../../screens/Admin/AdminManageQuestionnaires";
-
+import { USER_TYPE_CLINICIAN, USER_TYPE_ADMIN } from "../../utils/helper";
 
 const ContentPanel = (props) => {
-
+    // ========================================================================
+    // Content Panel definition for Clinician
+    // ========================================================================
     if (props.userType === USER_TYPE_CLINICIAN) {
+        switch (props.active) {
+            case 2:
+                return (<DoTheTestContainer/>);
+            case 3:
+                return (<InstructionsContainer/>);
+            case 4:
+                return (
+                    <QuestionnaireContainer
+                        questionnaireID={props.questionnaireID}
+                        redirectURL="/clinician"
+                    />
+                );
+            case 5:
+                return (
+                    <ViewQuestionnaireContainer
+                        questionnaireID={props.questionnaireID}
+                    />
+                );
+            case 6:
+                return (<ShareContainer/>);
+            default:
+                return (<AdminManageQuestionnaires/>);
 
-        if (props.active === 2) {
-            return (
-                <DoTheTestContainer/>
-            );
-        } else if (props.active === 3) {
-            return (
-                <InstructionsContainer/>
-            );
-        } else if (props.active === 4) {
-            return (
-                <QuestionnaireContainer
-                    questionnaireID={props.questionnaireID}
-                    redirectURL="/clinician"
-                />
-            );
-        } else if (props.active === 5) {
-            return (
-                <ViewQuestionnaireContainer
-                    questionnaireID={props.questionnaireID}
-                />
-            );
-        } else if (props.active === 6) {
-            return (
-                <ShareContainer/>
-            );
-        } else {
-            return (
-                <ManageQuestionnaires/>
-            );
         }
+
+    // ========================================================================
+    // Content Panel definition for admin
+    // ========================================================================
     } else if (props.userType === USER_TYPE_ADMIN) {
-        if (props.active === 2) {
-            return (
-                <div className="content-container">
-                    <SSQInstructionsList />
-                </div>
-            );
-        } else if (props.active === 3) {
-            return (
-                <div className="content-container">
-                    <CountryList />
-                </div>
-            );
-        } else if (props.active === 4) {
-            return (
-                <div className="content-container">
+        switch (props.active) {
+            case 2:
+                return (<SSQInstructionsList/>);
+            case 3:
+                return (<CountryList/>);
+            case 4:
+                return (
                     <QuestionnaireContainer
                         questionnaireID={props.questionnaireID}
                         redirectURL="/admin/Questionnaires"
                     />
-                </div>
-            );
-        } else if (props.active === 5) {
-            return (
-                <div className="content-container">
+                );
+            case 5:
+                return (
                     <AdminViewStandardQuestionnaire
                         questionnaireID={props.questionnaireID}
                     />
-                </div>
-            );
-        } else if (props.active === 6) {
-            return (
-                <div className="content-container">
+                );
+            case 6:
+                return (
                     <InstructionContainer
                         instructionType={props.instructionType}
                     />
-                </div>
-            );
-        } else if (props.active === 7) {
-            return (
-                <div className="content-container">
-                    <OrganisationList countryName={props.countryName} />
-                </div>
-            )
-        } else if (props.active === 8) {
-            return (
-                <div className="content-container">
-                    <OrganisationContainer organName={props.organName} />
-                </div>
-            )
+                );
+            case 7:
+                return (<OrganisationList countryName={props.countryName}/>);
+
+            case 8:
+                return (<OrganisationContainer organName={props.organName}/>);
+            default:
+                return (<AdminManageQuestionnaires/>);
+
         }
-        else {
-            return (
-                <div className="content-container">
-                    <AdminManageQuestionnaires />
-                </div>
-            );
-        }
+
+    // ========================================================================
+    // If not admin or clinician
+    // ========================================================================
     } else {
         return null;
     }
--- src/screens/Admin/index.js
+++ src/screens/Admin/index.js
@@ -3,11 +3,17 @@ import InstructionContainer from "./SSQInstructionContainer";
 import OrganisationContainer from "./OrganisationContainer";
 import CountryList from "./CountryList";
 import QuestionnaireContainer from "../Common/QuestionnaireContainer";
+import AdminManageQuestionnaires from "./AdminManageQuestionnaires";
+import SSQInstructionsList from "./SSQInstructionsList";
+import OrganisationList from "./OrganisationList";
 
 export {
     AdminViewStandardQuestionnaire,
     InstructionContainer,
     OrganisationContainer,
     CountryList,
-    QuestionnaireContainer
+    QuestionnaireContainer,
+    AdminManageQuestionnaires,
+    SSQInstructionsList,
+    OrganisationList,
 };
