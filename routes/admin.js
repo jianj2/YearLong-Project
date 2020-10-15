8401896eb7858df67f69dac283b640092338491c
caryjpaepen <42157570+caryjpaepen@users.noreply.github.com>
2020-10-15 13:07:01 +1100

    Hot-fix loading instruction

--- routes/admin.js
+++ routes/admin.js
@@ -19,9 +19,9 @@ const admin_controller = require('../controller/admin_controller.js');
 router.post('/login', admin_controller.loginAdmin);
 router.post('/instruction/:type', admin_controller.authorize, admin_controller.updateInstructionByType);
 
-router.get('/specificInstruction/:instructionType', admin_controller.authorize, admin_controller.getSpecificInstruction);
+router.get('/specificInstruction/:instructionType', admin_controller.getSpecificInstruction);
 router.get("/verifylogin/:token", admin_controller.verifyLogin);
-router.get("/instructionsSummary",admin_controller.authorize,admin_controller.getInstructionsSummary);
+router.get("/instructionsSummary",admin_controller.getInstructionsSummary);
 
 //router for getting the organisation information
 router.get("/country", admin_controller.authorize, admin_controller.getCountryList);
