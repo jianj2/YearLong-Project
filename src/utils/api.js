1ce9934412e420f0feb87f402bce99b11bb58574
Waqas Rehmani <dada.scene@hotmail.com>
2020-10-16 18:42:43 +1100

    Organise everything in the api.js file

--- src/utils/api.js
+++ src/utils/api.js
@@ -10,8 +10,7 @@
  * =============================================================================
  */
 
-import {getToken} from "./useAdminAuth";
-
+import { getToken } from "./useAdminAuth";
 
 const api = process.env.REACT_APP_SERVER || "http://localhost:3001";
 
@@ -19,12 +18,12 @@ const api = process.env.REACT_APP_SERVER || "http://localhost:3001";
 
 const header = {
     "Content-Type": "application/json",
-    Accept: "application/json",
+    Accept: "application/json"
 };
 
 let createHeader = (accessToken) => {
     return {
-        Authorization: `Bearer ${accessToken}`,
+        Authorization: `Bearer ${accessToken}`
     };
 };
 
@@ -38,12 +37,12 @@ const sendRequest = async (
     if (token) {
         headers = {
             ...header,
-            ...createHeader(token),
+            ...createHeader(token)
         };
     }
     let fetchOptions = {
         method,
-        headers,
+        headers
     };
     if (method === "POST") {
         fetchOptions = { ...fetchOptions, body: JSON.stringify(data) };
@@ -61,10 +60,121 @@ const sendRequest = async (
     }
 };
 
-// ================================================
-// Admin server calls
-// ================================================
+////////////////////////////////////////////////////////////////////////////////
+////                                   SHARE                                ////
+////////////////////////////////////////////////////////////////////////////////
+export const sendQuestionnaireData = async (data, shareId) => {
+    const url = `share/submit/${shareId}`;
+    return await sendRequest("POST", url, data);
+};
 
+////////////////////////////////////////////////////////////////////////////////
+////                              QUESTIONNAIRE                             ////
+////////////////////////////////////////////////////////////////////////////////
+// get specific questionnaire
+export const getQuestionnaireById = async (Id) => {
+    const url = `questionnaire/${Id}`;
+    return await sendRequest("GET", url);
+};
+
+// get standardised questionnaires
+export const getStandardisedQuestionnaires = async () => {
+    const url = `questionnaire/standardised`;
+    return await sendRequest("GET", url);
+};
+
+////////////////////////////////////////////////////////////////////////////////
+////                         ADMIN & QUESTIONNAIRE                          ////
+////////////////////////////////////////////////////////////////////////////////
+
+export const addStandardQuestionnaire = async () => {
+    const url = "questionnaire/addStandard";
+    return await sendRequest("POST", url, undefined, getToken());
+};
+
+//edit standard questionnaire
+export const editStandardQuestionnaire = async (questionnaire) => {
+    const url = "questionnaire/editStandard";
+    const data = {
+        questionnaire
+    };
+    return await sendRequest("POST", url, data, getToken());
+};
+
+export const adminCopyQuestionnaire = async (questionnaire) => {
+    const url = "questionnaire/copy";
+    const data = {
+        copyToCustomisedQuestionnaire: false,
+        questionnaire
+    };
+    return await sendRequest("POST", url, data);
+};
+
+export const deleteStandardQuestionnaire = async (questionnaireID) => {
+    const url = "questionnaire/deleteStandard";
+    const data = {
+        questionnaireID
+    };
+    return await sendRequest("POST", url, data, getToken());
+};
+
+////////////////////////////////////////////////////////////////////////////////
+////                        CLINICIAN & QUESTIONNAIRE                       ////
+////////////////////////////////////////////////////////////////////////////////
+
+export const addQuestionnaire = async (token, clinicianId) => {
+    const url = "questionnaire/add";
+    const data = {
+        clinicianId,
+        isStandard: false
+    };
+    return await sendRequest("POST", url, data, token);
+};
+
+// get clinician questionnaire list
+export const getClinicianQuestionnaires = async (accessToken, clinicianId) => {
+    const url = `questionnaire/clinician?clinicianId=${clinicianId}`;
+    return await sendRequest("GET", url, undefined, accessToken);
+};
+
+// Edit questionnaire.
+export const editQuestionnaire = async (token, questionnaire) => {
+    const url = "questionnaire/edit";
+    const data = {
+        questionnaire
+    };
+    return await sendRequest("POST", url, data, token);
+};
+
+//COPY questionnaire
+export const copyQuestionnaire = async (questionnaire, clinicianId) => {
+    const url = "questionnaire/copy";
+    const data = {
+        clinicianId,
+        copyToCustomisedQuestionnaire: true,
+        questionnaire
+    };
+    return await sendRequest("POST", url, data);
+};
+
+// Delete customised questionnaire.
+export const deleteQuestionnaire = async (token, CQid, clinicianId) => {
+    const url = "questionnaire/delete";
+    const data = {
+        CQid,
+        clinicianId
+    };
+    return await sendRequest("POST", url, data, token);
+};
+
+export const completeQuestionnaire = async (token, data) => {
+    const url = `clinician/complete-questionnaire/`;
+    return await sendRequest("POST", url, data, token);
+};
+
+////////////////////////////////////////////////////////////////////////////////
+////                                   ADMIN                                ////
+////////////////////////////////////////////////////////////////////////////////
 export const findPassword = async (email) => {
     const url =
         process.env.NODE_ENV === "production"
@@ -77,12 +187,12 @@ export const findPassword = async (email) => {
     const data = {
         client_id: client_id,
         email: email,
-        connection: "Username-Password-Authentication",
+        connection: "Username-Password-Authentication"
     };
     const fetchOptions = {
         method: "POST",
         headers: header,
-        body: JSON.stringify(data),
+        body: JSON.stringify(data)
     };
     await fetch(url, fetchOptions);
 };
@@ -97,148 +207,17 @@ export const verifyAdminLogin = async (token) => {
     return await sendRequest("GET", url);
 };
 
-export const sendQuestionnaireData = async (data, shareId) => {
-    const url = `share/submit/${shareId}`;
-    return await sendRequest("POST", url, data);
-};
-
-// ================================================
-// Clinician server calls
-// ================================================
-export const completeQuestionnaire = async (token, data) => {
-    const url = `clinician/complete-questionnaire/`;
-    return await sendRequest("POST", url, data, token);
-};
-
-// ================================================
-// Managing Questionnaire server calls
-// ================================================
-// add new questionnaire
-
-// export const getQuestionnaire = async (questionnaireID) =>{
-//     const url = `questionnaire/${questionnaireID}`
-//     return await sendRequest("GET", url);
-// }
-
-export const addQuestionnaire = async (token, clinicianId) => {
-    const url = "questionnaire/add";
-    const data = {
-        clinicianId,
-        isStandard: false,
-    };
-    return await sendRequest("POST", url, data, token);
-};
-
-export const addStandardQuestionnaire = async () => {
-    const url = "questionnaire/addStandard";
-    return await sendRequest("POST", url,undefined,getToken());
-};
-
-// delete customised questionnaire
-export const deleteQuestionnaire = async (token, CQid, clinicianId) => {
-    const url = "questionnaire/delete";
-    const data = {
-        CQid,
-        clinicianId,
-    };
-    return await sendRequest("POST", url, data, token);
-};
-
-//edit questionnaire
-
-export const editQuestionnaire = async (token, questionnaire) => {
-    const url = "questionnaire/edit";
-    const data = {
-        questionnaire,
-    };
-    return await sendRequest("POST", url, data, token);
-};
-
-//edit standard questionnaire
-
-export const editStandardQuestionnaire = async (questionnaire) => {
-    const url = "questionnaire/editStandard";
-    const data = {
-        questionnaire,
-    };
-    return await sendRequest("POST", url, data,getToken());
-};
-
-//COPY questionnaire
-export const copyQuestionnaire = async (questionnaire, clinicianId) => {
-    const url = "questionnaire/copy";
-    const data = {
-        clinicianId,
-        copyToCustomisedQuestionnaire: true,
-        questionnaire,
-    };
-    return await sendRequest("POST", url, data);
-};
-
-//admin COPY questionnaire
-export const adminCopyQuestionnaire = async (questionnaire) => {
-    const url = "questionnaire/copy";
-    const data = {
-        copyToCustomisedQuestionnaire: false,
-        questionnaire,
-    };
-    return await sendRequest("POST", url, data);
-};
-
-// get specific questionnaire
-
-export const getQuestionnaireById = async (Id) => {
-    const url = `questionnaire/${Id}`;
-    return await sendRequest("GET", url);
-};
-
-// get clinician questionnaire list
-
-export const getClinicianQuestionnaires = async (accessToken, clinicianId) => {
-    const url = `questionnaire/clinician?clinicianId=${clinicianId}`;
-    return await sendRequest("GET", url, undefined, accessToken);
-};
-
-// get standardised questionnaires
-export const getStandardisedQuestionnaires = async () => {
-    const url = `questionnaire/standardised`;
-    return await sendRequest("GET", url);
-};
-
-export const deleteStandardQuestionnaire = async (questionnaireID) => {
-    const url = "questionnaire/deleteStandard";
-    const data = {
-        questionnaireID,
-    };
-    return await sendRequest("POST", url, data,getToken());
-};
-
-// ================================================
-// Managing Share server calls
-// ================================================
-// get questionnaire ID using the Share ID.
-export const getShareDetails = async (shareId) => {
-    const url = `share/${shareId}`;
-    return await sendRequest("GET", url);
-}
-
-// Used to share a questionnaire.
-export const shareQuestionnaire = async (token, data) => {
-    const url = `clinician/share/`;
-    return await sendRequest("POST", url, data, token);
-}
 
 // get instruction based on type
 export const getSpecificInstruction = async (instructionType) => {
     const url = `admin/specificInstruction/${instructionType}`;
-    return await sendRequest("GET", url, undefined,getToken());
+    return await sendRequest("GET", url, undefined, getToken());
 };
 
 // get instructions summary including title and type
-
 export const getInstructionsSummary = async () => {
     const url = `admin/instructionsSummary`;
-    return await sendRequest("GET", url, undefined,getToken());
+    return await sendRequest("GET", url, undefined, getToken());
 };
 
 // update instruction by type
@@ -251,17 +230,32 @@ export const updateInstruction = async (type, data) => {
 export const getCountries = async () => {
     const url = `admin/country`;
     return await sendRequest("GET", url, undefined, getToken());
-}
+};
 
 // get organisations
 export const getOrganisations = async (countryName) => {
     const url = `admin/country/organisation/${countryName}`;
-    return await sendRequest("GET", url, undefined,getToken());
+    return await sendRequest("GET", url, undefined, getToken());
 };
 
 // get organisation's clinicians
 export const getOrganisationClinicians = async (organisationName) => {
     const url = `admin/organisation/clinician/${organisationName}`;
-    return await sendRequest("GET", url,undefined,getToken() );
+    return await sendRequest("GET", url, undefined, getToken());
 };
 
+
+
+////////////////////////////////////////////////////////////////////////////////
+////                                   SHARE                                ////
+////////////////////////////////////////////////////////////////////////////////
+export const getShareDetails = async (shareId) => {
+    const url = `share/${shareId}`;
+    return await sendRequest("GET", url);
+};
+
+// Used to share a questionnaire.
+export const shareQuestionnaire = async (token, data) => {
+    const url = `clinician/share/`;
+    return await sendRequest("POST", url, data, token);
+};
