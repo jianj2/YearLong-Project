0ba7ea53dc81607a20481a4c2b1ccbbb7201c0fa
Waqas Rehmani <dada.scene@hotmail.com>
2020-10-19 19:41:07 +1100

    set redirect_uri to "window.location.href"

--- src/screens/HomeClinician.js
+++ src/screens/HomeClinician.js
@@ -55,6 +55,14 @@ const HomeClinician = (props) => {
                     });
                     setToken(accessToken);
                 } catch (e) {
+
+                    // var newWin = window.open("clinician");
+                    // if(!newWin || newWin.closed || typeof newWin.closed=='undefined')
+                    // {
+                    //     //POPUP BLOCKED
+                    //     console.log("POPUP BLOCKED !!!!");
+                    // }
+
                     accessToken = await getTokenWithPopup({
                         // this is for the first time when some registers on localhost (
                         // localhost is treated differently by Auth0
@@ -62,6 +70,16 @@ const HomeClinician = (props) => {
                         scope: "read:current_user"
                     });
                     setToken(accessToken);
+
+
+
+                    console.log("accessToken", accessToken)
+                    // setToken(accessToken);
+
+
+
+
+
                 }
             };
             setAuth0Token();
@@ -70,7 +88,9 @@ const HomeClinician = (props) => {
 
         const fn = async () => {
             await loginWithRedirect({
-                redirect_uri: `${client}/clinician`
+                // redirect_uri: `${client}/clinician`
+                redirect_uri: window.location.href
+
                 //"http://localhost:3000/clinician", //TODO: figure out why window.location.pathname doesn't work
                 //redirect_uri: "https://d1hg2pgsuj0kio.cloudfront.net/clinician", //TODO: figure out why window.location.pathname doesn't work
                 //appState: { targetUrl: window.location.pathname},
