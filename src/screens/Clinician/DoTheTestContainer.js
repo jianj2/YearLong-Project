a70e15dfdedc64a7dc31be615a5c0dbe15afb922
Guang YANG <gyya@student.unimelb.edu.au>
2020-10-23 00:13:36 +1100

    remove review button at the top

--- src/screens/Clinician/DoTheTestContainer.js
+++ src/screens/Clinician/DoTheTestContainer.js
@@ -225,10 +225,6 @@ const DoTheTestContainer = () => {
                     <button className="button" onClick={prevStep}>
                         B A C K
                     </button>
-                    <button className="button" form="questionaire-container"
-                            type="submit">
-                        R E V I E W
-                    </button>
                 </div>
 
                 <Questionnaire
