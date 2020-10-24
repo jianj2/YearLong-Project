6e42ded7b5de579180965f6ab90104a6e66852a5
jianj2 <jianj2@student.unimelb.edu.au>
2020-10-25 02:25:41 +1100

    update the unittesting for frontend patientPage

--- src/testing/Sprint4/unittest_frontend_patientPage.py
+++ src/testing/Sprint4/unittest_frontend_patientPage.py
@@ -77,7 +77,29 @@ class unittest_frontend_patientPage(unittest.TestCase):
         time.sleep(2)
         assert self.driver.find_element_by_class_name("parents-detail-form")
 
-    def test_next_withInput_page2(self):
+    def test_next_withWrongInput_page2(self):
+        next1 = self.driver.find_element_by_class_name("subheader-container").find_element_by_class_name("button")
+        next1.click()
+        time.sleep(2)
+        name = self.driver.find_elements_by_class_name("MuiInputBase-input")[0]
+        name.send_keys("TestName")
+        time.sleep(2)
+        birth = self.driver.find_elements_by_class_name("MuiInputBase-input")[2]
+        birth.send_keys("aabbccdd")
+        time.sleep(2)
+        LDevice = self.driver.find_elements_by_class_name("MuiInputBase-input")[1]
+        LDevice.send_keys("None")
+        time.sleep(2)
+        RDevice = self.driver.find_elements_by_class_name("MuiInputBase-input")[3]
+        RDevice.send_keys("None")
+        time.sleep(2)
+        next2 = self.driver.find_element_by_class_name("parents-detail-form-submit-button").find_element_by_class_name(
+            "button")
+        next2.click()
+        time.sleep(2)
+        assert self.driver.find_element_by_class_name("parents-detail-form")
+
+    def test_next_withCorrectInput_page2(self):
         next1 = self.driver.find_element_by_class_name("subheader-container").find_element_by_class_name("button")
         next1.click()
         time.sleep(2)
@@ -223,7 +245,35 @@ class unittest_frontend_patientPage(unittest.TestCase):
         review = self.driver.find_element_by_id("review")
         review.click()
         time.sleep(2)
-        assert self.driver.find_element_by_class_name("review-submission")
+        assert self.driver.find_element_by_class_name("dothetest-container")
+
+    def test_review_answered_page3(self):
+        next1 = self.driver.find_element_by_class_name("subheader-container").find_element_by_class_name("button")
+        next1.click()
+        time.sleep(2)
+        name = self.driver.find_elements_by_class_name("MuiInputBase-input")[0]
+        name.send_keys("TestName")
+        time.sleep(2)
+        birth = self.driver.find_elements_by_class_name("MuiInputBase-input")[2]
+        birth.send_keys("19960829")
+        time.sleep(2)
+        LDevice = self.driver.find_elements_by_class_name("MuiInputBase-input")[1]
+        LDevice.send_keys("None")
+        time.sleep(2)
+        RDevice = self.driver.find_elements_by_class_name("MuiInputBase-input")[3]
+        RDevice.send_keys("None")
+        time.sleep(2)
+        next2 = self.driver.find_element_by_class_name("parents-detail-form-submit-button").find_element_by_class_name(
+            "button")
+        next2.click()
+        time.sleep(2)
+        select=self.driver.find_elements_by_class_name("MuiButtonBase-root")[0]
+        select.click()
+        time.sleep(2)
+        review = self.driver.find_element_by_id("review")
+        review.click()
+        time.sleep(2)
+        assert self.driver.find_element_by_class_name("dothetest-container")
 
     def test_instruction_page4(self):
         next1 = self.driver.find_element_by_class_name("subheader-container").find_element_by_class_name("button")
@@ -244,6 +294,12 @@ class unittest_frontend_patientPage(unittest.TestCase):
         next2 = self.driver.find_element_by_class_name("parents-detail-form-submit-button").find_element_by_class_name(
             "button")
         next2.click()
+        time.sleep(2)
+        select=self.driver.find_elements_by_class_name("MuiButtonBase-root")[0]
+        select.click()
+        time.sleep(2)
+        review = self.driver.find_element_by_id("review")
+        review.click()
         time.sleep(2)
         instruction = self.driver.find_element_by_class_name("subheader-container").find_elements_by_class_name(
             "button")[0]
@@ -272,6 +328,12 @@ class unittest_frontend_patientPage(unittest.TestCase):
             "button")
         next2.click()
         time.sleep(2)
+        select=self.driver.find_elements_by_class_name("MuiButtonBase-root")[0]
+        select.click()
+        time.sleep(2)
+        review = self.driver.find_element_by_id("review")
+        review.click()
+        time.sleep(2)
         back = self.driver.find_element_by_class_name("subheader-container").find_elements_by_class_name(
             "button")[1]
         back.click()
@@ -297,6 +359,12 @@ class unittest_frontend_patientPage(unittest.TestCase):
         next2 = self.driver.find_element_by_class_name("parents-detail-form-submit-button").find_element_by_class_name(
             "button")
         next2.click()
+        time.sleep(2)
+        select=self.driver.find_elements_by_class_name("MuiButtonBase-root")[0]
+        select.click()
+        time.sleep(2)
+        review = self.driver.find_element_by_id("review")
+        review.click()
         time.sleep(2)
         submit = self.driver.find_element_by_class_name("subheader-container").find_elements_by_class_name(
             "button")[2]
